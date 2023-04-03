ef492d52c1471226c87e03db32b0ef7d156fa3b5
Josh <jsh.grn@gmail.com>
2023-04-03 17:11:30 +0100

    configered eslint and prettier to work with tsx

--- App.tsx
+++ App.tsx
@@ -1,21 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-
+import { TileViewer } from './components/tileViewer';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Pathfinder Spell App!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-} 
+    return (
+        <View style={styles.container}>
+            <Text>Pathfinder Spell sApp!</Text>
+            <StatusBar style="auto" />
+            {/* <TileViewer /> */}
+        </View>
+    );
+}
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
 });
---
+++ components/tileViewer.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { ScrollView, Text } from 'react-native';
+
+const TileViewer = () => (
+    <ScrollView>
+        <Text>Text 1</Text>
+        <Text>Text 2</Text>
+        <Text>Text 3</Text>
+        <Text>Text 4</Text>
+    </ScrollView>
+);
+
+export { TileViewer };
