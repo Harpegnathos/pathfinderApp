2ce6aeb6808c37374d910b19b3a9a9cc8862ac49
Josh <jsh.grn@gmail.com>
2023-04-03 12:55:01 +0100

    Converted project to typescript and added spells.json

--- App.tsx
+++ App.tsx
@@ -1,6 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+
 export default function App() {
   return (
     <View style={styles.container}>
@@ -8,7 +9,7 @@ export default function App() {
       <StatusBar style="auto" />
     </View>
   );
-}
+} 
 
 const styles = StyleSheet.create({
   container: {
