1de7f79594817e5082456e8e36c1b0a71a5e1e19
Josh Green <jsh.grn@gmail.com>
2023-06-28 16:51:36 +0100

    💄 added style sheet for spell detail card

--- styleSheets/spellDetailsCard.styles.ts
+++ styleSheets/spellDetailsCard.styles.ts
@@ -1,8 +1,24 @@
 import { StyleSheet } from 'react-native';
 
 const styles = StyleSheet.create({
-    modalContent: {
-        backgroundColor: 'pink',
+    backgroundTint: {
+        height: '100%',
+        width: '100%',
+    },
+    modalView: {
+        margin: 20,
+        backgroundColor: 'lightblue',
+        borderRadius: 20,
+        padding: 35,
+        alignItems: 'center',
+        shadowColor: '#000',
+        shadowOffset: {
+            width: 0,
+            height: 2,
+        },
+        shadowOpacity: 0.25,
+        shadowRadius: 4,
+        elevation: 5,
     },
 });
 
--- styleSheets/spellPreview.styles.ts
+++ styleSheets/spellPreview.styles.ts
@@ -56,6 +56,28 @@ const styles = StyleSheet.create({
     textBold: {
         fontWeight: 'bold',
     },
+
+    // =========================================
+
+    backgroundTint: {
+        height: '100%',
+        width: '100%',
+    },
+    modalView: {
+        margin: 20,
+        backgroundColor: 'lightblue',
+        borderRadius: 20,
+        padding: 35,
+        alignItems: 'center',
+        shadowColor: '#000',
+        shadowOffset: {
+            width: 0,
+            height: 2,
+        },
+        shadowOpacity: 0.25,
+        shadowRadius: 4,
+        elevation: 5,
+    },
 });
 
 export { styles };
